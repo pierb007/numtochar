efd3439fa68315a65f41b7d70e9b531c7bf4ceca
Pier <james.pier@outlook.com>
2017-09-21 07:51:00 -0400

    Add project files.

---
+++ numtochar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numtochar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {}
+        public string WriteNumToChar(string str)
+        {
+            string returnStr = "";
+            foreach (char c in str)
+            {
+                returnStr = returnStr + (int)c + "";                
+            }
+            return returnStr;
+            Console.ReadLine();
+        }       
+    }
+}
